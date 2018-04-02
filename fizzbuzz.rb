7f273840e69c55360adda6fa108ab992f8794081
Ryan Barrar <Rbarrar17@gmail.com>
2018-04-02 10:15:44 -0400

    adding fizzbuzz.rb to repo

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+# fizzbuzz
+
+100.times do |i|
+  i +=1
+  if i % 15 == 0
+    puts "Fizzbuzz"
+  elsif i % 3 == 0
+    puts "Buzz"
+  elsif i % 5 == 0
+    puts "Fizz"
+  else
+    puts i
+  end
+end
